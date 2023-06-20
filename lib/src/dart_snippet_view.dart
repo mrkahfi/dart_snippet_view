8fd7a496da4a58ace218c024f3225a6e4a0e545f
Muhammad Rijalul Kahfi <kahfi@zero-one-group.com>
2023-06-20 10:21:27 +0700

    chore: update deps

--- lib/src/dart_snippet_view.dart
+++ lib/src/dart_snippet_view.dart
@@ -1,6 +1,6 @@
 import 'package:dart_style/dart_style.dart';
 import 'package:flutter/widgets.dart';
-import 'package:webviewx/webviewx.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class DartSnippetView extends StatefulWidget {
   final double height;
@@ -21,6 +21,8 @@ class DartSnippetView extends StatefulWidget {
 
 class _DartSnippetViewState extends State<DartSnippetView> {
   String? code;
+  late WebViewController controller;
+
   @override
   void initState() {
     if (widget.runFormatter) {
@@ -28,27 +30,44 @@ class _DartSnippetViewState extends State<DartSnippetView> {
     } else {
       code = widget.code;
     }
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadHtmlString(code ?? '');
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return WebViewX(
-      height: widget.height,
-      width: widget.width,
-      initialSourceType: SourceType.html,
-      onWebViewCreated: (controller) {
-        controller.loadContent(
-          'packages/dart_snippet_view/assets/code_viewer.html',
-          SourceType.html,
-          fromAssets: true,
-        );
-        // Image.asset('name',package: '',);
+    return WebViewWidget(controller: controller);
+    // return WebViewWidget(
+    //   height: widget.height,
+    //   width: widget.width,
+    //   onWebViewCreated: (controller) {
+    //     controller.loadContent(
+    //       'packages/dart_snippet_view/assets/code_viewer.html',
+    //       fromAssets: true,
+    //     );
+    //     // Image.asset('name',package: '',);
 
-        Future.delayed(const Duration(seconds: 1), () {
-          controller.callJsMethod('updateCodeContent', [code]);
-        });
-      },
-    );
+    //     Future.delayed(const Duration(seconds: 1), () {
+    //       controller.callJsMethod('updateCodeContent', [code]);
+    //     });
+    //   },
+    // );
   }
 }
